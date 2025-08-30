48af73408be375aa10b29e37fa166ef4586ffe2d
Petko Todorov <133591358+wngman889@users.noreply.github.com>
2025-08-30 14:22:58 +0300

    added models

---
+++ Models/Auction.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RealDarkBid.Models
+{
+    [Table("auction")]
+    public class Auction
+    {
+        public int ProductId { get; set; }
+
+        public string? ProductName { get; set; }
+
+        public string? Brand { get; set; }
+
+        public string? Category { get; set; }
+
+        public float? Price { get; set; }
+
+        public float? Rating { get; set; }
+
+        public string? Color { get; set; }
+
+        public string? Size { get; set; }
+
+        public int? UserId { get; set; }
+
+        public string? ImageUrl { get; set; }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RealDarkBid.Models
+{
+    [Table("users")]
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string? Username { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Password { get; set; }
+    }
+}
