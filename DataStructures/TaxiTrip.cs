8cfecf179d14f04791f6c34311842af52c643c36
Petko Todorov <133591358+wngman889@users.noreply.github.com>
2025-08-30 14:28:47 +0300

    Added the ml .net folders

---
+++ DataStructures/TaxiTrip.cs
@@ -0,0 +1,37 @@
+﻿
+using Microsoft.ML.Data;
+
+namespace Regression_TaxiFarePrediction.DataStructures
+{
+    public class TaxiTrip
+    {
+        [LoadColumn(0)]
+        public int UserID;
+
+        [LoadColumn(1)]
+        public int ProductID;
+
+        [LoadColumn(2)]
+        public string ProductName;
+
+        [LoadColumn(3)]
+        public string Brand;
+
+        [LoadColumn(4)]
+        public string Category;
+
+        [LoadColumn(5)]
+        public float Price;
+
+        [LoadColumn(6)]
+        public float Rating;
+
+        [LoadColumn(7)]
+        public string Color;
+
+        [LoadColumn(8)]
+        public string Size;
+
+
+    }
+}
---
+++ DataStructures/TestTaxiTrips.cs
@@ -0,0 +1,25 @@
+﻿namespace Regression_TaxiFarePrediction.DataStructures
+{
+    internal class SingleTaxiTripSample
+    {
+        internal static readonly TaxiTrip Trip1 = new TaxiTrip
+        {
+            //VendorId = "VTS",
+            //RateCode = "1",
+            //PassengerCount = 1,
+            //TripDistance = 10.33f,
+            //PaymentType = "CSH",
+            //FareAmount = 0 // predict it. actual = 29.5
+
+            UserID = 10,
+            ProductID = 1,
+            ProductName = "Shoes",
+            Brand = "Nike",
+            Category = "Men's Fashion",
+            Price = 5.5f,
+            Rating = 2,
+            Color = "Black",
+            Size = "L"
+        };
+    }
+}
